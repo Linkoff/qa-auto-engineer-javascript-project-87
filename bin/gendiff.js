6810955a34b3a57a4013a4e956d5a2e9028ccabb
Linkoff <linkov.ea@yandex.ru>
2026-08-12 14:41:08 +0300

    feat: add positional arguments and --format option to CLI

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,8 @@ import packageJson from '../package.json' with { type: 'json' }
 
 program
   .version(packageJson.version)
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
   .parse(process.argv)
