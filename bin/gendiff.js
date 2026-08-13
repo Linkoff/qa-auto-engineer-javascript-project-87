3ddeeaaf7e420f530870a49a6ea51dc70a8d4eb9
Linkoff <linkov.ea@yandex.ru>
2026-08-13 16:20:03 +0300

    feat: add file reading and JSON parsing to genDiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,7 @@
 
 import { program } from 'commander'
 import packageJson from '../package.json' with { type: 'json' }
+import genDiff from '../src/index.js'
 
 program
   .version(packageJson.version)
@@ -9,4 +10,8 @@ program
   .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2, options) => {
+    const result = genDiff(filepath1, filepath2)
+    console.log(result)
+  })
   .parse(process.argv)
--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+import { readFileSync } from 'fs'
+import { extname } from 'path'
+
+const parseFile = (filepath) => {
+  const content = readFileSync(filepath, 'utf-8')
+  const ext = extname(filepath).slice(1)
+
+  if (ext === 'json') {
+    return JSON.parse(content)
+  }
+  // позже добавим yaml, пока только json
+  throw new Error(`Unsupported file format: ${ext}`)
+}
+
 export default function genDiff(filepath1, filepath2) {
-  return 'Difference will be here'
+  const data1 = parseFile(filepath1)
+  const data2 = parseFile(filepath2)
+
+  return { data1, data2 }
 }
