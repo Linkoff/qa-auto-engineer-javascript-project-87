e3795d2cdde30d9d63bf2263235d291733bd0c01
Linkoff <linkov.ea@yandex.ru>
2026-08-19 16:24:37 +0300

    chore: ignore coverage directory in ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,15 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-]);
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  {
+    ignores: ['node_modules/**', 'coverage/**', 'dist/**', '__fixtures__/**'],
+  },
+])
